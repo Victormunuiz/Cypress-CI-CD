5bfcf2326377a90e56f37aff8ffc26e0ab0b1e20
victor muniz <victor.muniz@ultrawise.pt>
2025-03-19 14:55:44 +0000

    adicionar o teste de login automazado

---
+++ cypress/e2e/login automatizado.cy.js
@@ -0,0 +1,13 @@
+describe('Login automatizado', () => {
+  it('deve realizar login com sucesso', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    // Preenche o login
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+
+    // Valida se o usuário foi redirecionado à homepage
+    cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+  })
+})
